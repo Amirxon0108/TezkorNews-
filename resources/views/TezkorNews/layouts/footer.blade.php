b2c53e73a3d727692a5255246682ef7384a47076
amirdevvf <amirdevfullstack@gmail.com>
2026-01-16 22:34:46 +0300

    website alredey done

--- resources/views/TezkorNews/layouts/footer.blade.php
+++ resources/views/TezkorNews/layouts/footer.blade.php
@@ -116,10 +116,10 @@
 						</div>
 
 						<ul class="m-t--12">
-							@foreach( $categories as $category)
+							@foreach( $categories as $categor)
 							<li class="how-bor1 p-rl-5 p-tb-10">
 								<a href="#" class="f1-s-5 cl11 hov-cl10 trans-03 p-tb-8">
-									{{ $category->name }}({{ $category->articles_count }})
+									{{ $categor->name }}({{ $categor->articles_count }})
 								</a>
 							</li>
 							@endforeach
